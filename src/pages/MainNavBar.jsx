04d37bb7930738bc18ce19b70804df7beed9a79b
bhanu teja <2500090202@kluniversity.in>
2026-02-21 10:01:54 +0530

    modified MainNavBar.jsx

--- src/pages/MainNavBar.jsx
+++ src/pages/MainNavBar.jsx
@@ -17,7 +17,7 @@ const MainNavBar = () => {
       <div className="navbar-container">
         <div className="navbar-logo">
           <Link to="/" className="logo-link">
-            KL Event Management System
+            KL Event Management System - 2500090202
           </Link>
         </div>
         <ul className="nav-menu">
